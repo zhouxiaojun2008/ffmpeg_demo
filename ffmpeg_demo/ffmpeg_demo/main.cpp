ca54c12ac5dd0a5252ef2a0bd88e6757d4192f54
XiaojunZhou <XiaojunZhou@pptv.com>
2018-07-26 16:50:52 +0800

    解码 yuv保存

--- ffmpeg_demo/ffmpeg_demo/main.cpp
+++ ffmpeg_demo/ffmpeg_demo/main.cpp
@@ -98,6 +98,139 @@ void print_error(const char* name, int err)
     av_log(NULL, AV_LOG_ERROR, "%s: %s %d\n", name, errbuf_ptr,err);
 }
 
+static int video_decode_example(const char *input_filename)
+{
+    AVCodec *codec = NULL;
+    AVCodecContext *ctx= NULL;
+    AVCodecParameters *origin_par = NULL;
+    AVFrame *fr = NULL;
+    uint8_t *byte_buffer = NULL;
+    AVPacket pkt;
+    AVFormatContext *fmt_ctx = NULL;
+    int number_of_written_bytes;
+    int video_stream;
+    int got_frame = 0;
+    int byte_buffer_size;
+    int i = 0;
+    int result;
+    int end_of_stream = 0;
+
+    result = avformat_open_input(&fmt_ctx, input_filename, NULL, NULL);
+    if (result < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Can't open file\n");
+        return result;
+    }
+
+    result = avformat_find_stream_info(fmt_ctx, NULL);
+    if (result < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Can't get stream info\n");
+        return result;
+    }
+
+    video_stream = av_find_best_stream(fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, -1, NULL, 0);
+    if (video_stream < 0) {
+        av_log(NULL, AV_LOG_ERROR, "Can't find video stream in input file\n");
+        return -1;
+    }
+
+    origin_par = fmt_ctx->streams[video_stream]->codecpar;
+
+    codec = avcodec_find_decoder(origin_par->codec_id);
+    if (!codec) {
+        av_log(NULL, AV_LOG_ERROR, "Can't find decoder\n");
+        return -1;
+    }
+
+    ctx = avcodec_alloc_context3(codec);
+    if (!ctx) {
+        av_log(NULL, AV_LOG_ERROR, "Can't allocate decoder context\n");
+        return AVERROR(ENOMEM);
+    }
+
+    result = avcodec_parameters_to_context(ctx, origin_par);
+    if (result) {
+        av_log(NULL, AV_LOG_ERROR, "Can't copy decoder context\n");
+        return result;
+    }
+
+    result = avcodec_open2(ctx, codec, NULL);
+    if (result < 0) {
+        av_log(ctx, AV_LOG_ERROR, "Can't open decoder\n");
+        return result;
+    }
+
+    fr = av_frame_alloc();
+    if (!fr) {
+        av_log(NULL, AV_LOG_ERROR, "Can't allocate frame\n");
+        return AVERROR(ENOMEM);
+    }
+
+    //byte_buffer_size = av_image_get_buffer_size(ctx->pix_fmt, ctx->width, ctx->height, 16);
+    //byte_buffer = av_malloc(byte_buffer_size);
+    if (!byte_buffer) {
+        av_log(NULL, AV_LOG_ERROR, "Can't allocate buffer\n");
+        return AVERROR(ENOMEM);
+    }
+
+    printf("#tb %d: %d/%d\n", video_stream, fmt_ctx->streams[video_stream]->time_base.num, fmt_ctx->streams[video_stream]->time_base.den);
+    i = 0;
+    av_init_packet(&pkt);
+    /*do {
+        if (!end_of_stream)
+            if (av_read_frame(fmt_ctx, &pkt) < 0)
+                end_of_stream = 1;
+        if (end_of_stream) {
+            pkt.data = NULL;
+            pkt.size = 0;
+        }
+        if (pkt.stream_index == video_stream || end_of_stream) {
+            got_frame = 0;
+            if (pkt.pts == AV_NOPTS_VALUE)
+                pkt.pts = pkt.dts = i;
+            result = avcodec_decode_video2(ctx, fr, &got_frame, &pkt);
+            if (result < 0) {
+                av_log(NULL, AV_LOG_ERROR, "Error decoding frame\n");
+                return result;
+            }
+            if (got_frame) {
+                number_of_written_bytes = av_image_copy_to_buffer(byte_buffer, byte_buffer_size,
+                    (const uint8_t* const *)fr->data, (const int*) fr->linesize,
+                    ctx->pix_fmt, ctx->width, ctx->height, 1);
+                if (number_of_written_bytes < 0) {
+                    av_log(NULL, AV_LOG_ERROR, "Can't copy image to buffer\n");
+                    return number_of_written_bytes;
+                }
+                printf("%d, %10"PRId64", %10"PRId64", %8"PRId64", %8d, 0x%08lx\n", video_stream,
+                    fr->pts, fr->pkt_dts, fr->pkt_duration,
+                    number_of_written_bytes, av_adler32_update(0, (const uint8_t*)byte_buffer, number_of_written_bytes));
+            }
+            av_packet_unref(&pkt);
+            av_init_packet(&pkt);
+        }
+        i++;
+    } while (!end_of_stream || got_frame);*/
+
+    av_packet_unref(&pkt);
+    av_frame_free(&fr);
+    avcodec_close(ctx);
+    avformat_close_input(&fmt_ctx);
+    avcodec_free_context(&ctx);
+    av_freep(&byte_buffer);
+    return 0;
+}
+
+int api_h264_test_main(int argc, char **argv)
+{
+    argv[1] = "超级翁婿(第09集)_超清.MP4";
+
+    av_register_all();
+
+    if (video_decode_example(argv[1]) != 0)
+        return 1;
+
+    return 0;
+}
+
 /*
 av_seek_frame 参数flag为0，则返回当前时间后面的第一个关键帧,如果后面没有关键帧则返回-1
 单AVSEEK_FLAG_BACKWARD参数，则返回前面的一个关键帧
@@ -106,7 +239,9 @@ AVSEEK_FLAG_ANY参数代表任意帧
 */
 int main(int argc, char **argv)
 {
-    char *url = "超级翁婿(第09集)_超清.MP4";
+   // return api_h264_test_main(argc,argv);
+
+    char *url = "pptiyu.mp4";
     av_register_all();
     int err = 0;
 
@@ -151,11 +286,9 @@ int main(int argc, char **argv)
     }
 
     AVCodecContext *pCodecCtx = avcodec_alloc_context3(pCodec);
-    if(avcodec_copy_context(pCodecCtx, pCodecCtxOrig) != 0) {
-        fprintf(stderr, "Couldn't copy codec context");
-        return -1; // Error copying codec context
-    }
     
+    avcodec_parameters_to_context(pCodecCtx, pFormatContext->streams[video_index]->codecpar);
+   
     err = avcodec_open2(pCodecCtx,pCodec,NULL);
     if ( err != 0) {
         print_error("avcodec_open2",err);
@@ -173,6 +306,7 @@ int main(int argc, char **argv)
     char *buffer = (char*)av_malloc_array(numBytes,1);
     
     FILE *pf = fopen("save.264","wb");
+    FILE *pf_yuv = fopen("save.yuv","wb");
 
     char logname[64] = "";
     strcat(logname,url);
@@ -182,21 +316,56 @@ int main(int argc, char **argv)
 
     AVPacket *pkt = av_packet_alloc();
     int count = 1;
+    int count_frame = 1;
     int got_picture_ptr = 0;
     bool is_over = false;
     AVBitStreamFilterContext* h264bsfc = av_bitstream_filter_init("h264_mp4toannexb");
+
+    char* buf = new char[pCodecCtx->height * pCodecCtx->width * 3 / 2];
+    memset(buf, 0, pCodecCtx->height * pCodecCtx->width * 3 / 2);
+    int height = pCodecCtx->height;
+    int width = pCodecCtx->width;
+
     while (1)
     {
         err = av_read_frame(pFormatContext,pkt);
         if (err == 0){
             if (pkt->stream_index == video_index) { //packet pts dts的值是通过pFormatContext->streams[video_index]->time_base的单位，实际上就是mp4 track box里面的timescale
              
-                fprintf(plog,"video seg bef seq: %-6d, dts: %-8lld,dts-time: %-8lld, pts: %-8lld, size: %-6d, flags: %-2d, duration: %lld.\n",count,
-                    pkt->dts,av_rescale_q(pkt->dts,pFormatContext->streams[video_index]->time_base,av_make_q(1, 1000)),pkt->pts,pkt->size,pkt->flags,av_rescale_q(pkt->duration,pFormatContext->streams[video_index]->time_base,av_make_q(1, 1000)));
-#if 0   
+                fprintf(plog,"video packet bef seq: %-6d, dts: %-8lld,dts-time: %-8lld, pts: %-8lld, size: %-6d, flags: %-2d, duration: %lld %lld.\n",count,
+                    pkt->dts,av_rescale_q(pkt->dts,pFormatContext->streams[video_index]->time_base,av_make_q(1, 1000)),pkt->pts,pkt->size,pkt->flags,pkt->duration,av_rescale_q(pkt->duration,pFormatContext->streams[video_index]->time_base,av_make_q(1, 1000)));
+
+                av_bitstream_filter_filter(h264bsfc, pFormatContext->streams[video_index]->codec, NULL, &pkt->data, &pkt->size, pkt->data, pkt->size, 0);
+                fwrite(pkt->data,1,pkt->size,pf);
+
+                avcodec_decode_video2(pCodecCtx,pFrame, &got_picture_ptr,pkt); 
+                if (got_picture_ptr)
+                {
+                    fprintf(plog,"video frame bef seq: %-6d, pts:%-9lld,packet dts: %-8lld,packet pts: %-8lld, flags: %-2d,pkt duration: %lld.\n",count_frame,
+                        pFrame->pts,pFrame->pkt_dts,pFrame->pkt_pts,pFrame->flags,pFrame->pkt_duration);
+     
+                    int a = 0, i;
+                    for (i = 0; i<height; i++)
+                    {
+                        memcpy(buf + a, pFrame->data[0] + i * pFrame->linesize[0], width);
+                        a += width;
+                    }
+                    for (i = 0; i<height / 2; i++)
+                    {
+                        memcpy(buf + a, pFrame->data[1] + i * pFrame->linesize[1], width / 2);
+                        a += width / 2;
+                    }
+                    for (i = 0; i<height / 2; i++)
+                    {
+                        memcpy(buf + a, pFrame->data[2] + i * pFrame->linesize[2], width / 2);
+                        a += width / 2;
+                    }
+                    fwrite(buf, 1, pCodecCtx->height * pCodecCtx->width * 3 / 2, pf_yuv);
+                }
+  #if 0
                 if (pkt->flags == 1)
                 {
-                    fprintf(plog,"video seg end seq: %-6d, dts: %-8lld, pts: %-8lld, size: %-6d, flags: %-2d, duration: %lld.\n",count++,
+                    fprintf(plog,"video packet end seq: %-6d, dts: %-8lld, pts: %-8lld, size: %-6d, flags: %-2d, duration: %lld.\n",count++,
                         av_rescale_q(pkt->dts,pFormatContext->streams[video_index]->time_base,av_make_q(1, 1000)),
                         av_rescale_q(pkt->pts,pFormatContext->streams[video_index]->time_base,av_make_q(1, 1000)),
                         pkt->size,
@@ -205,10 +374,10 @@ int main(int argc, char **argv)
                 }
                 
 #endif
-                //av_bitstream_filter_filter(h264bsfc, pFormatContext->streams[video_index]->codec, NULL, &packet.data, &packet.size, packet.data, packet.size, 0);
-
-                //fwrite(packet.data,1,packet.size,pf);
-                //avcodec_decode_video2(pCodecCtx,pFrame,&got_picture_ptr,&packet);
+                count ++;
+                count_frame ++;
+                
+              
             }else if (pkt->stream_index == audio_index) {
 #if 0
                 fprintf(plog,"audio seg bef seq: %-6d, dts: %-8lld, pts: %-8lld, size: %-6d, flags: %-2d, duration: %lld.\n",count,
@@ -233,9 +402,10 @@ int main(int argc, char **argv)
             #define AVSEEK_FLAG_ANY      4 ///< seek to any frame, even non-keyframes
             #define AVSEEK_FLAG_FRAME    8 ///< seeking based on frame number
             */
-            if (is_over == true)
-                break;
+            //if (is_over == true)
             print_error("av_read_frame",err);
+            break;
+            
             int64_t seektime = 10;
             err = av_seek_frame(pFormatContext,video_index,av_rescale_q(seektime,av_make_q(1, 1),pFormatContext->streams[video_index]->time_base),AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_ANY); 
             if (err != 0)
@@ -247,6 +417,7 @@ int main(int argc, char **argv)
             is_over = true;
             continue;
         }
+        
         av_packet_unref(pkt);
     }
 
@@ -256,6 +427,7 @@ int main(int argc, char **argv)
 
     fclose(pf);
     fclose(plog);
+    fclose(pf_yuv);
     
     getchar();
 }
